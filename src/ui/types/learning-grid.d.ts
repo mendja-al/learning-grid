1f7df9a2ffd836d2ae2a2f2bc8b7ad589b247bd5
Ermir Suldashi <ermirsuldashi@gmail.com>
2019-04-18 14:38:00 +0200

    added JSDoc params to the learning grid definitions

--- src/ui/types/learning-grid.d.ts
+++ src/ui/types/learning-grid.d.ts
@@ -1,4 +1,7 @@
-/** Turns on a pixel in our grid. */
+/** Turns on a pixel in our grid.
+ * @param {number} x the horizontal coordinate of the pixel, needs to be a postive integer from 0 to 7
+ * @param {number} y the vertical coordinate of the pixel, needs to be a postive integer from 0 to 7
+ */
 declare function pixelOn(x: number, y: number): void;
 
 /** Resets the pixel grid. */
