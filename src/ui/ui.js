8901d9cc84f0c022ccb872c67b3085ccd8825dff
Ermir Suldashi <ermirsuldashi@gmail.com>
2019-04-25 14:07:06 +0200

    changed a bit of the styling with regards to the cell borders and added settings to set the width of the borders and width of the filled cell

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -13,19 +13,24 @@ window.initGrid = (hPixels,vPixels) => {
     verPixels = vPixels;
     pixelWidth = Math.floor(canvasWidth/horPixels);
     pixelHeight = Math.floor(canvasHeight/verPixels);
-    ctx.strokeStyle = "#ccc";
     ctx.fillStyle = "#fff";
-    ctx.lineWidth = 2;
     ctx.fillRect(0,0,canvasWidth,canvasHeight);
     for(var i=0;i<horPixels;i++) {
         for(var j=0;j<verPixels;j++) {
-            ctx.beginPath();
-            ctx.moveTo(i*pixelWidth,(j+1)*pixelHeight);
-            ctx.lineTo((i+1)*pixelWidth,(j+1)*pixelHeight);
-            ctx.lineTo((i+1)*pixelWidth,j*pixelHeight);
-            ctx.stroke();
+            drawGridCell(i,j);   
         }
     }
+}
+
+function drawGridCell(x,y) {
+    let borderWidth = 0.02;
+    let startX = x*pixelWidth;
+    let startY = y*pixelHeight;
+    ctx.fillStyle = "#eee";
+    ctx.fillRect(startX,startY,pixelWidth,borderWidth*pixelHeight);
+    ctx.fillRect(startX,startY+(pixelHeight*(1-borderWidth)),pixelWidth,borderWidth*pixelHeight);
+    ctx.fillRect(startX,startY,borderWidth*pixelWidth,pixelHeight);
+    ctx.fillRect(startX+(pixelWidth*(1-borderWidth)),startY,pixelWidth*borderWidth,pixelHeight);
 }
 
 initGrid(horPixels,verPixels);
@@ -34,10 +39,11 @@ window.colorize = colorize;
 window.resetColors = resetColors;
 
 window.pixelOn = (x,y) => {
-    let startX = x*pixelWidth;
-    let startY = y*pixelHeight;
+    let borderWidth = 0.10; //in percent of total width
+    let startX = (x+borderWidth)*pixelWidth;
+    let startY = (y+borderWidth)*pixelHeight;
     ctx.fillStyle = "#000";
-    ctx.fillRect(startX,startY,pixelWidth,pixelHeight);
+    ctx.fillRect(startX,startY,pixelWidth*(1-(2*borderWidth)),pixelHeight*(1-(2*borderWidth)));
 }
 
 window.runIde = () => {
