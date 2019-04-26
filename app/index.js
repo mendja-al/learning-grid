f9b1aecd092e114c5052bda8ecb19913f986375e
Ermir Suldashi <ermirsuldashi@gmail.com>
2019-04-26 14:27:04 +0200

    moved the frontend from a static file to a handlebars template, IDE to iframe

--- app/index.js
+++ app/index.js
@@ -5,15 +5,22 @@ const app = express();
 const server = require("http").createServer(app);
 const bodyParser = require('body-parser')
 const path = require("path");
+const exphbs = require("express-handlebars");
 
 const port = 8888;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/public', express.static(path.resolve(__dirname,"..",'public')));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.get("*",(req,res) => {
-	res.sendFile(path.resolve("public/index.html"));
+app.get("/",(req,res) => {
+	res.render("index");
+});
+
+app.get("/frame",(req,res) => {
+	res.render("frame");
 });
 
 app.set("x-powered-by",false);
